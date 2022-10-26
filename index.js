efb93c2a3a80b12bd61a542225292de7de9a9bb4
Soto <rodrigo.soto@airesdigital.com.br>
2022-10-25 21:02:07 -0300

    Alteração 3 Origin

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ setInterval(async () => {
     } catch (error) {
         return error;
     }
-}, 10000);
+}, 5000);
 
 async function getCurrentQuote(ticker, callback) {
     const response = await fetch("https://finance.yahoo.com/quote/" + ticker + ".sa/")
