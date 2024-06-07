6a9953bf5f37c22535dc45a9983227f974f49645
Soto <rodrigo.soto@airesdigital.com.br>
2024-06-06 21:00:03 -0300

    Correção B3SA3

--- ticker-enum.js
+++ ticker-enum.js
@@ -2,7 +2,7 @@ const tickerEnum = {
     PRIO3 :	'hrt-petroleo-on-nm',
     RAPT4 :	'randon-part-pn-n1',
     ELET3 :	'eletrobras-on',
-    B3SA3 :	'brasil-on',
+    B3SA3 :	'bmfbovespa-on-nm',
     TGMA3 :	'tegma-on-nm',
     CSMG3 :	'copasa-on',
     CPFE3 :	'cpfl-energia-on-nm',
